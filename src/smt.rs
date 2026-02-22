8f940f858ff0ee823d0b15114f5b169159da3cba
71104 <a71104@gmail.com>
2026-02-22 20:08:08 +0100

    Remove any concurrency strategies and delegate all synchronization to the caller.

--- src/smt.rs
+++ src/smt.rs
@@ -5,8 +5,6 @@ use ff::{Field, PrimeField};
 use primitive_types::U256;
 use std::cmp::Ordering;
 use std::fmt::Debug;
-use std::ops::Deref;
-use std::sync::{Mutex, atomic::AtomicU64};
 
 /// Indicates that a type is suitable for storage in a memory-mapped region.
 pub trait Stored: Sized + Copy + Clone + 'static {}
@@ -56,14 +54,7 @@ impl PartialOrd for StoredScalar {
 #[repr(C)]
 struct Node<const W: usize> {
     /// Tracks how many other nodes refer to this node.
-    ref_count1: u64,
-
-    /// Tracks how many `NodeRef`s refer to this node.
-    ///
-    /// NOTE: the `ref_count2`s of all nodes must be set to zero when constructing a `Tree` on a
-    /// data slice, regardless of the state of the rest of the slice. This is achieved by
-    /// `Tree::clear_ref_count2`.
-    ref_count2: u64,
+    ref_count: u64,
 
     /// The hash of the node.
     ///
@@ -84,23 +75,15 @@ impl<const W: usize> Node<W> {
         }
     }
 
-    fn init_with_hash(&mut self, hash: Scalar, children: [Scalar; W]) {
-        self.ref_count1 = 0;
-        self.ref_count2 = 0;
+    fn init_with_hash(&mut self, hash: Scalar, children: &[Scalar; W]) {
+        self.ref_count = 0;
         self.hash = hash.into();
         self.children = children.map(StoredScalar::from);
     }
 
-    fn init(&mut self, children: [Scalar; W]) {
+    fn init(&mut self, children: &[Scalar; W]) {
         let hash = Self::hash_node(&children);
         self.init_with_hash(hash, children);
-    }
-
-    unsafe fn to_mut(&self) -> &mut Self {
-        unsafe {
-            // SAFETY: this function unsafely makes `self` mutable by definition.
-            &mut *(std::ptr::from_ref(self) as *mut Self)
-        }
     }
 
     fn erase(&mut self) {
@@ -112,43 +95,17 @@ impl<const W: usize> Node<W> {
         self.hash.to_scalar() == Scalar::ZERO
     }
 
-    fn ref1(&mut self) {
-        self.ref_count1 += 1;
-    }
-
-    fn unref1(&mut self) -> bool {
-        self.ref_count1 -= 1;
-        self.ref_count1 == 0
-    }
-
-    fn ref_count2(&self) -> &AtomicU64 {
-        unsafe {
-            // SAFETY: the pointer supplied to `AtomicU64::from_ptr` is 8-byte aligned because:
-            //  - the `data` slice managed by the parent `Tree` is required to be 8-byte aligned;
-            //  - the padded header size is a multiple of 8 bytes;
-            //  - the only field before `ref_count2` is `ref_count1`, which takes 8 bytes.
-            &*AtomicU64::from_ptr(std::ptr::from_ref(&self.ref_count2) as *mut u64)
-        }
-    }
-
-    fn clear_ref_count2(&self) {
-        self.ref_count2()
-            .store(0, std::sync::atomic::Ordering::Relaxed);
-    }
-
-    fn ref2(&self) {
-        self.ref_count2()
-            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-    }
-
-    fn unref2(&self) -> bool {
-        self.ref_count2()
-            .fetch_sub(1, std::sync::atomic::Ordering::AcqRel)
-            == 1
-    }
-
     fn is_reffed(&self) -> bool {
-        self.ref_count1 + self.ref_count2().load(std::sync::atomic::Ordering::Acquire) > 0
+        self.ref_count > 0
+    }
+
+    fn r#ref(&mut self) {
+        self.ref_count += 1;
+    }
+
+    fn unref(&mut self) -> bool {
+        self.ref_count -= 1;
+        self.ref_count == 0
     }
 
     fn hash(&self) -> Scalar {
@@ -173,41 +130,6 @@ impl<const W: usize> PartialEq for Node<W> {
 impl<const W: usize> Eq for Node<W> {}
 
 #[derive(Debug)]
-struct NodeRef<'tree, 'node, const W: usize, const H: usize> {
-    tree: &'tree Tree<'node, W, H>,
-    node: &'node Node<W>,
-}
-
-impl<'tree, 'node, const W: usize, const H: usize> NodeRef<'tree, 'node, W, H> {
-    fn new(tree: &'tree Tree<'node, W, H>, node: &'node Node<W>) -> Self {
-        node.ref2();
-        Self { tree, node }
-    }
-}
-
-impl<'tree, 'node, const W: usize, const H: usize> Clone for NodeRef<'tree, 'node, W, H> {
-    fn clone(&self) -> Self {
-        Self::new(self.tree, self.node)
-    }
-}
-
-impl<'tree, 'node, const W: usize, const H: usize> Deref for NodeRef<'tree, 'node, W, H> {
-    type Target = Node<W>;
-
-    fn deref(&self) -> &Self::Target {
-        self.node
-    }
-}
-
-impl<'tree, 'node, const W: usize, const H: usize> Drop for NodeRef<'tree, 'node, W, H> {
-    fn drop(&mut self) {
-        if self.node.unref2() {
-            self.tree.maybe_deallocate_node(self.node);
-        }
-    }
-}
-
-#[derive(Debug)]
 #[repr(C)]
 struct TreeHeader {
     size: u64,
@@ -229,36 +151,27 @@ impl TreeHeader {
 /// The underlying hash map has open addressing with quadratic probing and is implemented as a
 /// simple node array over the memory-mapped region.
 #[derive(Debug)]
-pub struct Tree<'data, const W: usize, const H: usize> {
+pub struct Tree<'a, const W: usize, const H: usize> {
     /// Refers to the memory-mapped region.
-    data: &'data mut [u8],
-
-    /// Guards mutable access to certain parts of `data`.
-    ///
-    /// TODO: more details.
-    ///
-    /// NOTE: the `ref_count2` fiels of each node are handled with atomic operations, which have
-    /// internal mutability, so those are not considered mutable access to the data slice and are
-    /// normally carried out outside of the mutex lock.
-    mutex: Mutex<()>,
+    data: &'a mut [u8],
 }
 
-impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
+impl<'a, const W: usize, const H: usize> Tree<'a, W, H> {
     const MAX_LOAD_FACTOR_NUMERATOR: usize = 3;
     const MAX_LOAD_FACTOR_DENOMINATOR: usize = 4;
 
     /// Calculates the space allocated for the header.
-    fn padded_header_size() -> usize {
+    const fn padded_header_size() -> usize {
         std::mem::size_of::<TreeHeader>().next_multiple_of(8)
     }
 
     /// Calculates the space allocated for every node.
-    fn padded_node_size() -> usize {
+    const fn padded_node_size() -> usize {
         std::mem::size_of::<Node<W>>().next_multiple_of(8)
     }
 
     /// Returns a reference to the header.
-    fn header(&self) -> &'data TreeHeader {
+    fn header(&self) -> &TreeHeader {
         unsafe {
             // SAFETY: the data slice gets checked extensively upon construction of the `Tree`, it's
             // guaranteed to have enough space for the header.
@@ -266,48 +179,34 @@ impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
         }
     }
 
-    fn clear_ref_count2(&mut self) {
-        let capacity = self.capacity();
-        let data: &mut [u8] = self.data;
-        let offset = Self::padded_header_size();
-        for i in 0..capacity {
-            let offset = offset + i * Self::padded_node_size();
-            let node = unsafe {
-                // SAFETY: we're changing neither mutability nor lifetime, just reinterpreting the
-                // bytes for the node. The data slice gets checked extensively upon construction of
-                // the `Tree` and is guaranteed to have `capacity` nodes.
-                &mut *(data.as_ptr().add(offset) as *mut Node<W>)
-            };
-            node.clear_ref_count2();
-        }
-    }
-
-    fn raw_node<'tree: 'data>(&'tree self, i: usize) -> &'data Node<W> {
-        let data: &'data [u8] = self.data;
+    /// Returns an immutable reference to the node at the i-th slot.
+    fn node(&self, i: usize) -> &Node<W> {
         let offset = Self::padded_header_size() + i * Self::padded_node_size();
         unsafe {
             // SAFETY: we're changing neither mutability nor lifetime, just reinterpreting the bytes
             // for the node. The caller is assumed to provide a valid index `i`, in which case
             // `offset` will point to a valid node within the data slice.
-            &*(data.as_ptr().add(offset) as *const Node<W>)
+            &*(self.data.as_ptr().add(offset) as *const Node<W>)
         }
     }
 
-    fn node<'tree: 'data>(&'tree self, i: usize) -> NodeRef<'tree, 'data, W, H> {
-        let node = self.raw_node(i);
-        NodeRef::new(self, node)
+    /// Returns a mutable reference to the node at the i-th slot.
+    fn node_mut(&mut self, i: usize) -> &mut Node<W> {
+        let offset = Self::padded_header_size() + i * Self::padded_node_size();
+        unsafe {
+            // SAFETY: we're changing neither mutability nor lifetime, just reinterpreting the bytes
+            // for the node. The caller is assumed to provide a valid index `i`, in which case
+            // `offset` will point to a valid node within the data slice.
+            &mut *(self.data.as_ptr().add(offset) as *mut Node<W>)
+        }
     }
 
-    fn get_node_by_hash<'tree: 'data>(
-        &'tree self,
-        hash: Scalar,
-    ) -> Option<NodeRef<'tree, 'data, W, H>> {
+    fn find_node(&self, hash: Scalar) -> Option<&Node<W>> {
         let mask = self.capacity() as u64 - 1;
         let mut i = (utils::scalar_to_u256(hash) & U256::from(mask)).as_u64();
         let mut j = 0;
         loop {
-            let index = ((i + j) & mask) as usize;
-            let node = self.node(index);
+            let node = self.node(((i + j) & mask) as usize);
             if node.is_empty() {
                 return None;
             }
@@ -319,49 +218,8 @@ impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
         }
     }
 
-    fn allocate_node<'tree: 'data>(
-        &'tree self,
-        children: [Scalar; W],
-    ) -> NodeRef<'tree, 'data, W, H> {
-        let hash = Node::<W>::hash_node(&children);
-        if let Some(node) = self.get_node_by_hash(hash) {
-            return node;
-        }
-        let mask = self.capacity() as u64 - 1;
-        let mut i = (utils::scalar_to_u256(hash) & U256::from(mask)).as_u64();
-        let mut j = 0;
-        let _guard = self.mutex.lock().unwrap();
-        loop {
-            let node = self.raw_node(((i + j) & mask) as usize);
-            if node.is_empty() || !node.is_reffed() {
-                unsafe {
-                    // SAFETY: mutating the node is OK here because we're working on an unused slot
-                    // and we've got the mutex lock, so no other thread can be working on it.
-                    node.to_mut()
-                }
-                .init_with_hash(hash, children);
-                return NodeRef::new(self, node);
-            }
-            j += 1;
-            i += j;
-        }
-    }
-
-    fn maybe_deallocate_node(&self, node: &'data Node<W>) {
-        let _guard = self.mutex.lock().unwrap();
-        if !node.is_reffed() {
-            let node_mut = unsafe {
-                // SAFETY: the node has both reference counts sets to 0 (as per `is_reffed`) so it's
-                // undiscoverable. No other thread can be reading it. We've got the mutex lock so no
-                // other thread can be probing it either.
-                node.to_mut()
-            };
-            node_mut.erase();
-        }
-    }
-
     /// REQUIRES: `data` MUST be 8-byte aligned.
-    pub fn from_data(data: &'data mut [u8]) -> Result<Self> {
+    pub fn from_data(data: &'a mut [u8]) -> Result<Self> {
         let min_size = Self::padded_header_size()
             + (H * Self::MAX_LOAD_FACTOR_DENOMINATOR / Self::MAX_LOAD_FACTOR_NUMERATOR)
                 .next_power_of_two()
@@ -373,10 +231,7 @@ impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
                 min_size
             ));
         }
-        let mut tree = Self {
-            data,
-            mutex: Mutex::default(),
-        };
+        let tree = Self { data };
         let capacity = tree.capacity();
         if capacity != capacity.next_power_of_two() {
             return Err(anyhow!(
@@ -384,7 +239,13 @@ impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
                 capacity
             ));
         }
-        tree.clear_ref_count2();
+        Ok(tree)
+    }
+
+    /// REQUIRES: `data` MUST be 8-byte aligned.
+    pub fn new(data: &'a mut [u8]) -> Result<Self> {
+        let tree = Self::from_data(data)?;
+        // TODO: initialize.
         Ok(tree)
     }
 
@@ -400,29 +261,31 @@ impl<'data, const W: usize, const H: usize> Tree<'data, W, H> {
         (self.data.len() - Self::padded_header_size()) / Self::padded_node_size()
     }
 
-    // TODO
+    pub fn root_hash(&self) -> Scalar {
+        self.header().root_hash()
+    }
 }
 
-impl<'data, const H: usize> Tree<'data, 2, H> {
+impl<'a, const H: usize> Tree<'a, 2, H> {
     pub fn lookup(&self, key: Scalar) -> Scalar {
-        let mut node = self.get_node_by_hash(self.header().root_hash()).unwrap();
+        let mut node = self.find_node(self.header().root_hash()).unwrap();
         for i in (1..H).rev() {
             let bit = xits::and1(xits::shr(key, i)).to_repr()[0];
             let child_hash = node.child(bit as usize);
-            node = self.get_node_by_hash(child_hash).unwrap();
+            node = self.find_node(child_hash).unwrap();
         }
         let bit = xits::and1(key).to_repr()[0];
         node.child(bit as usize)
     }
 }
 
-impl<'data, const H: usize> Tree<'data, 3, H> {
+impl<'a, const H: usize> Tree<'a, 3, H> {
     pub fn lookup(&self, key: Scalar) -> Scalar {
-        let mut node = self.get_node_by_hash(self.header().root_hash()).unwrap();
+        let mut node = self.find_node(self.header().root_hash()).unwrap();
         for i in (1..H).rev() {
             let trit = xits::mod3(xits::div_pow3(key, i)).to_repr()[0];
             let child_hash = node.child(trit as usize);
-            node = self.get_node_by_hash(child_hash).unwrap();
+            node = self.find_node(child_hash).unwrap();
         }
         let trit = xits::mod3(key).to_repr()[0];
         node.child(trit as usize)
@@ -432,12 +295,15 @@ impl<'data, const H: usize> Tree<'data, 3, H> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::testing::parse_scalar;
 
     #[test]
     fn test_from_data() {
-        let mut data = [0u8; 57384];
-        let tree = Tree::<2, 256>::from_data(&mut data).unwrap();
-        // TODO
+        const HEADER_SIZE: usize = Tree::<2, 256>::padded_header_size();
+        const NODE_SIZE: usize = Tree::<2, 256>::padded_node_size();
+        let mut data = [0u8; HEADER_SIZE + 512 * NODE_SIZE];
+        let tree = Tree::<2, 256>::new(&mut data).unwrap();
+        assert_eq!(tree.root_hash(), parse_scalar(""));
     }
 
     // TODO
