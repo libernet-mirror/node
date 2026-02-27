531e9890b60688c0198ecfc2fe6fe8cde2c34a48
71104 <a71104@gmail.com>
2026-02-27 17:33:32 +0100

    Bugfix.

--- src/store.rs
+++ src/store.rs
@@ -511,7 +511,7 @@ impl<H: HeaderData, T: NodeData> MappedHashSet<H, T> {
                 return Some(value);
             }
             let k = self.get_slot_index(node.hash());
-            if k <= i || k > j {
+            if j > i && (k <= i || k > j) || (k <= i && k > j) {
                 *self.node_mut(i as usize) = *node;
                 i = j;
             }
